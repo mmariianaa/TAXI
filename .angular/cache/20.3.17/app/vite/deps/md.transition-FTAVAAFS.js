041bcf19fad1a3b60e9095c420bb1c2779e13d04
mmariianaa <24190463@utcalvillo.edu.mx>
2026-02-24 13:37:23 -0600

    Intento de loguin
    Hice un intento de loguin, no es funcional solo es la vista

---
+++ .angular/cache/20.3.17/app/vite/deps/md.transition-FTAVAAFS.js
@@ -0,0 +1,12 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-3G75OWGI.js";
+import "./chunk-UPPBUXP3.js";
+import "./chunk-PMTKRSGE.js";
+import "./chunk-QEE7QVES.js";
+import "./chunk-4554YRK6.js";
+import "./chunk-2H3NLAAY.js";
+import "./chunk-QHQP2P2Z.js";
+export {
+  mdTransitionAnimation
+};
